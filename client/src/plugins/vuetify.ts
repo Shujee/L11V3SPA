06c4d2e6547c4288739c09632799fb6ec67206f9
Syed Shujaat Hussain <shujaat@hotmail.com>
2024-10-15 16:24:05 +0500

    - imported colors module in vuetify.js

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -10,6 +10,7 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/util/colors'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -23,7 +24,7 @@ export default createVuetify({
       dark: {
         colors: {
           background: 'ffffff',
-          primary: 'ffffff',
+          primary: colors.indigo.base,
           secondary: 'ffffff',
           'on-background': 'ffffff',
           'on-primary': 'ffffff',
